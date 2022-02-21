20ab97c9412b8ee31028871f634992c4dcb6b136
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2022-02-21 08:40:34 +0530

    NEW: Add Bootstrap Framework

--- market.py
+++ market.py
@@ -1,13 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home_page():
-    return "<h1>Home Page</h1>"
-
-
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h1>About Page ({username})</h1>'
+    return render_template('home.html')
