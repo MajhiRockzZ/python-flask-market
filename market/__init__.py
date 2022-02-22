99b369092494d2050975bd0f4c7f35cdade4404e
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2022-02-22 19:40:37 +0530

    NEW: Add Flask Forms

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '4765678d65e0c56ff813dcd2'
 db = SQLAlchemy(app)
 
 from market import routes
